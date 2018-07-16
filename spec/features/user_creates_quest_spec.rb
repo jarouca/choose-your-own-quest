335a53665ac4f7c3da52731ae871f7e1d7866396
Jonathan Arouca <jonathanarouca@gmail.com>
2018-07-15 22:57:51 -0400

    edit user creates quest spec

--- spec/features/user_creates_quest_spec.rb
+++ spec/features/user_creates_quest_spec.rb
@@ -20,19 +20,31 @@ feature 'user creates a quest' do
     fill_in 'Title', with: 'The Quest For The Lost Sock'
 
     click_button 'Create Quest'
-    expect(page).to have_content('Quest successfully created')
+    expect(page).to have_content('Quest successfully created.')
   end
 
   scenario 'authorized user does not provide required quest information' do
+    login_as(admin_user, :scope => :admin_user)
+    visit root_path
+    click_link 'Create Quest'
 
+    click_button 'Create Quest'
+    expect(page).to have_content('Please provide a title for your quest.')
+    expect(page).to_not have_content('Quest successfully created.')
   end
 
   scenario 'unauthorized user unable to click create quest link' do
+    login_as(non_admin_user, :scope => :non_admin_user)
+    visit root_path
 
+    expect(page).to_not have_link('Create Quest')
   end
 
   scenario 'unathorized user unable to access quest#new page' do
+    login_as(non_admin_user, :scope => :non_admin_user)
+    visit new_quest_path
 
+    expect(page).to have_content('404 Not Found')
   end
 
 end
